4bc2bb0bc92ea36f0a9b9c2fa628351cef8dfdfe
V5行为验证 <support@verify5.com>
2020-06-26 12:02:22 +0800

    Update Verify.cs

--- Verify5_Demo/Verify.cs
+++ Verify5_Demo/Verify.cs
@@ -14,16 +14,18 @@ namespace Verify5_Demo
         /// </summary>
         /// <param name="token">前端验证时使用的Token</param>
         /// <param name="verifyid">前端生成的uuid</param>
-        public void ServerVerify(String token, String verifyid)
+        /// <param name="userId">自定义参数，添加自定义参数后在风险告警和数据分析中可以关联到业务数据</param>
+        /// <param name="userId">自定义参数，添加自定义参数后在风险告警和数据分析中可以关联到业务数据</param>	    
+        public void ServerVerify(String token, String verifyid , String userId, String userId)
         {
             String timestamp = Program.UnixTimestamp();
             SortedDictionary<String, String> dictionary = new SortedDictionary<String, String>();
             dictionary.Add("token", token);
             dictionary.Add("verifyid", verifyid);
             dictionary.Add("timestamp", timestamp);
-            // 以下是自定义参数，添加自定义参数后在风险告警和数据分析中可以关联到业务数据
-		    dictionary.Add("userId":"testuser");
-		    dictionary.Add("orderId":"OD22ce32d");
+
+	    dictionary.Add("userId", userId);
+	    dictionary.Add("orderId", orderId);
             
             String apiUrl = Program.appHost + "/openapi/verify";
             try
